7e136dcca3043787dc6f016286db506f2eeb2d3e
Henrique Kriguer <henriquekriguer@gmail.com>
2023-03-31 08:32:37 -0300

    inclusao desenhoConceitual.excalidraw

--- src/JasonParser.java
+++ src/JasonParser.java
@@ -13,30 +13,21 @@ public class JasonParser {
     public List<Map<String, String>> parse(String json) {
         Matcher matcher = REGEX_ITEMS.matcher(json);
         if (!matcher.find()) {
-
             throw new IllegalArgumentException("Não encontrou items.");
         }
-
         String[] items = matcher.group(1).split("\\},\\{"); //separa a coleção em cada array de filme
-
         List<Map<String, String>> dados = new ArrayList<>();
 
         for (String item : items) {
-
             Map<String, String> atributosItem = new HashMap<>();
-
             Matcher matcherAtributosJson = REGEX_ATRIBUTOS_JSON.matcher(item);
             while (matcherAtributosJson.find()) {
                 String atributo = matcherAtributosJson.group(1);
                 String valor = matcherAtributosJson.group(2);
                 atributosItem.put(atributo, valor);
             }
-
             dados.add(atributosItem);
         }
-
         return dados;
     }
-
-
 }
